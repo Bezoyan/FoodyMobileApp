a8eb4fb35fe5d8cb797d4b582b4be248bdd138ec
Bezoyan <zoya1905bezoyan@gmail.com>
2020-03-22 00:37:15 +0400

    First commit + navigation between diffret pages

---
+++ lib/dummy_data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'a1', title: 'Armenian', color: Colors.yellow),
+  Category(id: 'b1', title: 'Italian', color: Colors.green),
+  Category(
+    id: 'a2',
+    title: 'Humburger',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'b2',
+    title: 'Mexican',
+    color: Colors.red,
+  ),
+  Category(id: 'a3', title: 'Armenian', color: Colors.yellow),
+  Category(id: 'b3', title: 'Italian', color: Colors.green),
+  Category(
+    id: 'a4',
+    title: 'Indian',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'b4',
+    title: 'Mexican',
+    color: Colors.red,
+  ),
+];
