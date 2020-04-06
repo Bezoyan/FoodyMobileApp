9241fe397e1a28d4e4373856d84649302f9dfa5a
Bezoyan <zoya1905bezoyan@gmail.com>
2020-04-06 18:10:39 +0400

    Add Mail detail screen with ingredients and making stapes

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-            title: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-            subhead: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-            subtitle: TextStyle(
+            body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            title: TextStyle(
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
                 fontWeight: FontWeight.bold)),
